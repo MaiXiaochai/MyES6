dae22783b1b013d7bfd7f9a7d0ff285d48ac4169
maixiaochai <maixhaichai@outlook.com>
2020-09-28 20:15:07 +0800

    *)ES6基础版完成(非全部知识)

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/*
+* --------------------------------------
+* @File       : index.js
+* @Author     : maixiaochai
+* @Email      : maixiaochai@outlook.com
+* @CreatedOn  : 2020/9/28 19:53
+* --------------------------------------
+*/
+
+const PI = 31.4;
+const sum = (x, y) => x + y;
+const {
+    name,
+    age
+} = {
+    name: 'maixiaochai',
+    age: 18
+}
